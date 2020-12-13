1b56cd9b42a54e4825e0f2dc8a1dcd4e9dc34e85
balaJS <vvbala1995@gmail.com>
2020-12-13 14:15:58 +0530

    resolve conflicts2.

---
+++ public/javascripts/frondend.js
@@ -0,0 +1,11 @@
+var mp3bot = {};
+
+const msf_songs = new MSFmultiSelect(
+  document.querySelector('.multi-select'),
+  {
+    theme: 'theme2',
+    // appendTo: '.multi-select',
+    selectAll: true,
+    searchBox: true,
+  }
+);
