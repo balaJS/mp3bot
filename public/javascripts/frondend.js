6f389270ae65b93343163d11f38a9620b1b30deb
balaJS <vvbala1995@gmail.com>
2020-12-29 22:50:50 +0530

    List folder and songs name done.

--- public/javascripts/frondend.js
+++ public/javascripts/frondend.js
@@ -5,6 +5,52 @@ class Mp3bot {
     $offlineSourceForm.addEventListener('submit', sourceFormSubmit);
   }
 
+  templateCreation(rawRes) {
+    var res = JSON.parse(rawRes);
+    var folderHtml = this.songsFolderListTemplate(res.dir);
+    var songsHtml = this.songsListTemplate(res.files);
+    var title = res.title;
+    var template = `<div class="source-wrapper__folder-wrapper js-folder-wrapper">
+      <h2 class="source-wrapper__folder-wrapper__title" data-src="${title}">${title}</h2>
+      <div class="source-wrapper__folder-wrapper__songs">${songsHtml}</div>
+      <div class="source-wrapper__folder-wrapper__folders">${folderHtml}</div>
+    </div>
+    `;
+
+    document.getElementsByClassName('js-source-wrapper')[0].innerHTML += template;
+  }
+
+  songsFolderListTemplate(childDir) {
+    // TODO: need to setup recursion for child folders.
+    const self = this;
+    var dirHtml = '', songsHtml = '';
+    Object.keys(childDir).forEach(function(key) {
+      let dir = childDir[key];
+      songsHtml = self.songsListTemplate(dir.files);
+      dirHtml += `<div class="source-wrapper__folder-wrapper js-child-folder-wrapper">
+        <h2 class="source-wrapper__folder-wrapper__title" data-src="${dir.title}">${dir.title}</h2>
+        <div class="source-wrapper__folder-wrapper__songs">${songsHtml}</div>
+      </div>
+      `;
+    });
+    return dirHtml;
+  }
+
+  songsListTemplate(files = []) {
+    var filesHtml = '';
+    files.forEach(function(file) {
+      filesHtml += `<div class="source-wrapper__folder-wrapper__song js-song" data-src="${file}">
+        <span class="song">${file}</span>
+      </div>`;
+    });
+    return filesHtml;
+  }
+
+  playSong() {
+    const $player = document.getElementsByClassName('js-player')[0];
+    // $player.
+  }
+
   sourceFormSubmit(self, ev) {
     ev.preventDefault();
     const $this = ev.target;
@@ -17,19 +63,16 @@ class Mp3bot {
     // $url.value = '';
   }
 
-  filterSourcePath(path = '') {
-    // Linux OS
-    if (path.indexOf('file:///') !== -1) {
-      return path;
-    }
-    return false;
+  filterSourcePath(path = '~/Music') {
+    return path;
   }
 
   store(args = {}) {
     const data = {
       'method': 'POST',
       'path': '/store',
-      'data': args
+      'data': args,
+      'cb': this.templateCreation.bind(this)
     };
 
     this.makeReq(data);
@@ -41,6 +84,7 @@ class Mp3bot {
     path = location.origin + path;
     let contentType = req['contentType'] ? req['contentType'] : 'application/json;charset=UTF-8';
     let data = req['data'] ? req['data'] : {};
+    const cb = req['cb'];
 
     const xhr = new window.XMLHttpRequest();
     xhr.open(method, path, true);
@@ -49,6 +93,9 @@ class Mp3bot {
 
     xhr.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        if (typeof cb === 'function') {
+          cb(this.responseText);
+        }
         console.log('INFO', this.responseText);
       }
     };
--- routes/bot.js
+++ routes/bot.js
@@ -7,6 +7,27 @@ const url = 'mongodb://localhost:27017/mp3bot';
 const client = new dbClient(url, {useUnifiedTopology: true});
 const assert = require('assert');
 
+// get files from inserted path.
+const fs = require('fs');
+
+function get_list(path = '') {
+  if (!path) { return; }
+  var files = fs.readdirSync(path);
+  var fileList = {
+    dir: {},
+    files: []
+  };
+  files.forEach(function(file) {
+    if (file.endsWith('.mp3')) {
+      fileList.files.push(file);
+    } else if (file.search(/\./) === -1) {
+      fileList.dir[path + file] = get_list(path + file + '/');
+    }
+  });
+  fileList['title'] = path;
+  return fileList;
+}
+
 function save(data) {
   client.connect(function(err, client) {
     const db = client.db();
@@ -24,7 +45,7 @@ router.post('/store', function(req, res, next) {
   const formData = req.body;
   save(formData);
   console.log(formData.path);
-  return res.send('ok');
+  return res.send(get_list(formData['path']));
 });
 
 module.exports = router;
